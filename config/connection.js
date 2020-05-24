c1f49a724e2db4427fa348746aa5ddb0cc123d70
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-24 15:15:08 -0500

    set up the database connection

--- config/connection.js
+++ config/connection.js
@@ -3,3 +3,26 @@
 // Inside the connection.js file, setup the code to connect Node to MySQL.
 
 // Export the connection.
+
+// set up the MySQL connection
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+});
+
+//Make connection
+connection.connect(function(err){
+    if (err) {
+        console.log("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId)
+});
+
+//export the connection 
+module.exports = connection;
