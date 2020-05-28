7d7c50c90fabd8ea894fbb352a0ea1014b4b3a28
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-28 07:51:27 -0500

    make server connection

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,3 +6,18 @@
 
 
 // Create the router for the app, and export the router at the end of your file.
+
+var express = require("express");
+
+var router = express.Router();
+
+
+//imports the burger file 
+var burger = require ('../models/burger.js')
+
+//Create the needed Routes
+
+
+
+//Export module
+module.exports = router;
