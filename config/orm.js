751c6b5133ed38f80f73c23f45e5af863d8c8b4e
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-28 18:21:33 -0500

    set up print ?'s and objToSql  functions in the orm file

--- config/orm.js
+++ config/orm.js
@@ -13,6 +13,31 @@
 //import the connection to mysql
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(mun){
+    var arr = [];
+
+    for (var i = 0; i < num; i++){
+        arr.push("?")
+    }
+    return arr.toString();
+}
+
+function objToSql(ob){
+    var arr = [];
+
+    for(var key in ob) {
+        var value = ob[key];
+
+        if(Object.hasOwnProperty.call(ob, key)){
+            if(typeof value === "string" && value.indexOf(" ") >= 0){
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
 function selectAll() {
 
 }
