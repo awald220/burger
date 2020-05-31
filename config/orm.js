8d57f7eb27fffdc8bb74091d627936d6c8aef60b
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-30 20:53:14 -0500

    add queries to orm file

--- config/orm.js
+++ config/orm.js
@@ -38,16 +38,53 @@ function objToSql(ob){
     return arr.toString();
 }
 
-function selectAll() {
+var orm = {
+    all: function(tableInput, cb){
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
+    },
+    create: function(table, cols, vals, cb){
+        var queryString = "INSERT INTO" + table;
 
-}
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") "
+        
+        console.log(queryString);
 
-function insertOne() {
+        connection.query(queryString, vals, function(err, result){
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    
+    update: function(table, objColVals, condition, cb){
+        var queryString = "UPDATE " + table;
 
-}
+        queryString += "SET ";
+        queryString += objToSql(objColVals);
+        queryString += "WHERE ";
+        queryString += condition;
 
-function updateOne() {
-
+        console.log(queryString);
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+    
 }
 
 
