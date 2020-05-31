6b61bd8104c00d474dc6e53d2744fde5cec3cd79
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-30 21:12:01 -0500

    add devoured ajax call

--- models/burger.js
+++ models/burger.js
@@ -6,7 +6,27 @@
 
 // Export at the end of the burger.js file.
 
-//Import ORM
+$(".devour-it").on("click", function(event){
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevour");
+
+    var newDevourState = {
+        devoured: newDevour
+    };
+
+    $.ajax("/api/burgers" + id, {
+        type: "PUT", 
+        data: newDevourState
+    }).then (
+        function(){
+            console.log("changed state to", newDevour)
+            //Reload the page to get the update
+            location.reload();
+        }
+    );
+});
+
+
 $(".create-form").on("submit", function(event){
     event.preventDefault();
 
