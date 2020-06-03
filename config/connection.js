058c921556b788c1c560e7d3a6abaaa662c4b67e
Ashley Waldner <ashleywaldner22@gmail.com>
2020-06-02 19:54:55 -0500

    remove broken code blocks clean up code

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,3 @@
-// Create a connection.js file inside config directory.
-
-// Inside the connection.js file, setup the code to connect Node to MySQL.
-
-// Export the connection.
-
-// set up the MySQL connection
-// var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password",
-//     database: "burgers_db"
-// });
-
-// //Make connection
-// connection.connect(function(err){
-//     if (err) {
-//         console.log("error connecting: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId)
-// });
-
-// //export the connection for orm
-// module.exports = connection;
-
-
 var mysql = require('mysql');
 var connection;
 
