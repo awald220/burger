ce4bd6a4e86b4983c2105d8588061fd2c653c5f0
Ashley Waldner <ashleywaldner22@gmail.com>
2020-06-04 07:43:26 -0500

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express()
 
