c4737430539b06401bf40f00c5dbc9429f2ab6ff
Ashley Waldner <ashleywaldner22@gmail.com>
2020-06-04 20:22:08 -0500

    change connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
-var mysql = require('mysql');
-var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'burgers_db'
-    });
-};
+var mysql      = require('mysql');
 
-connection.connect();
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'burger_db'
+});
+ 
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
+
 module.exports = connection;
