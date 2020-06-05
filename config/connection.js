848aa7a96316324cbde2e6a416e7f14a8b1ca6fd
Ashley Waldner <ashleywaldner22@gmail.com>
2020-06-05 08:40:20 -0500

    add jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,17 @@
 var mysql = require("mysql");
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -18,4 +31,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
+connection.connnect()
 module.exports = connection;
