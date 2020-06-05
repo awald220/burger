3af8884ef8f6b64a49d0be850956e6c24afd14f6
Ashley Waldner <ashleywaldner22@gmail.com>
2020-06-05 08:20:55 -0500

    change sql connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,21 @@
-var mysql = require('mysql');
-var connection;
+var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-}
-else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'burgers_db'
-    });
-};
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+});
 
-connection.connect();
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
 module.exports = connection;
