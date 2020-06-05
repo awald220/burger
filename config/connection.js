8ccb9c3da360f77eeba578549eb9425781030eb2
Ashley Waldner <ashleywaldner22@gmail.com>
2020-06-05 18:55:33 -0500

    fix connnection

--- config/connection.js
+++ config/connection.js
@@ -13,23 +13,6 @@ if(process.env.JAWSDB_URL){
   });
 }
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
 // Export connection for our ORM to use.
 connection.connnect()
 module.exports = connection;
